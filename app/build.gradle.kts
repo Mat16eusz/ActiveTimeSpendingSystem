2b493ccd85cdab33f30635b342baecc1b948340b
Mateusz Jasiak <mateusz.jasiak.dev@gmail.com>
2024-01-12 17:54:13 +0100

    featureTest: Test implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -105,6 +105,10 @@ dependencies {
 
     // Tests
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.mockito:mockito-core:5.8.0")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
+    testImplementation("io.mockk:mockk:1.13.8")
+    testImplementation("androidx.arch.core:core-testing:2.2.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
