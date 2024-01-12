d946ac44f4a49453d6ff105908be960dd251b5ba
Mateusz Jasiak <mateusz.jasiak.dev@gmail.com>
2024-01-12 17:56:13 +0100

    featureTest: Library update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,15 +71,15 @@ dependencies {
     // Base
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
     // Dagger2
     implementation("com.google.dagger:dagger:2.48")
     implementation("com.google.dagger:dagger-android:2.48")
     implementation("com.google.dagger:dagger-android-support:2.48")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
     kapt("com.google.dagger:dagger-android-processor:2.48")
     kapt("com.google.dagger:dagger-compiler:2.48")
     kapt("com.google.dagger:dagger-android-support:2.48")
@@ -94,7 +94,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
 
     // Google SignIn
-    implementation("com.google.android.gms:play-services-base:18.2.0")
+    implementation("com.google.android.gms:play-services-base:18.3.0")
     implementation("com.google.android.gms:play-services-auth:20.7.0")
     implementation("com.google.firebase:firebase-messaging-ktx:23.4.0")
 
--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -44,7 +44,7 @@ dependencies {
     // Base
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
     // Dagger2
